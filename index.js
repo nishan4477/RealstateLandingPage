aaccf86656934f4c57dda2dbd85990389ba33d6e
Nishan4477 <nishansakha@gmail.com>
2023-12-20 13:48:33 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+$(window).ready(function() {
+
+    
+
+    $('.mobile-navbar-btn').click(function() {
+        
+        $('.header').toggleClass('active');
+        $('body').toggleClass('overflow');
+
+    });
+
+
+//     $('.property-card-slider').slick({
+//         infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+//       });
+    
+    
+})
